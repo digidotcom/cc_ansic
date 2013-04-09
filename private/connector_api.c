9e97c52636ef71380e29f37ec5eb7aca77a08564
Sebastian Pastor <sebastian.pastor@digi.com>
2013-04-09 23:03:29 +0200

    Init properly connector_handle in connector_init().
    	- malloc_cb() function does not initialize the allocated memory to 0x00 (only if DEBUG is defined). The memset must be explicitly done or structure's pointers may (or may not...) be equal to NULL. Before this change, it might work in some environments if malloc_cb() returns a memory block that is zeroed, but that was not guaranteed in any way.
    	- This assumes that NULL = 0x00, it migth cause problems in platforms on which this is not true... maybe it should be "memset(handle, (int) NULL, sizeof *connector_handle)" instead, but that might cause problems with other structure's members that do need to be initialized to 0x00.
    	- Fixes: https://jira.digi.com/browse/IC4C-64

--- private/connector_api.c
+++ private/connector_api.c
@@ -350,6 +350,7 @@ connector_handle_t connector_init(connector_callback_t const callback)
 #endif
 
         COND_ELSE_GOTO(status == connector_working, done);
+		memset(handle, 0x00, sizeof *connector_handle); /* Init structure, all pointers to NULL */
         connector_handle = handle;
     }
 
