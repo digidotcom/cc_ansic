8256e51e5eda21a611e6799eecad32b95788f619
Vidya Shankar <vshankar@digi.com>
2013-04-12 10:27:07 -0400

    send_data/get_file.py is updated to address IC4C-71
    Signed-off-by: Vidya Shankar <vshankar@digi.com>

--- public/run/samples/send_data/get_file.py
+++ public/run/samples/send_data/get_file.py
@@ -18,39 +18,40 @@
 import httplib
 import base64
 import sys
+import re
 
 def Usage():
     print 'Usage: get_file.py <username> <password> <device_id>\n'
-   
+
 def GetMessage(username, password, device_id):
     # create HTTP basic authentication string, this consists of
     # "username:password" base64 encoded
     auth = base64.encodestring("%s:%s"%(username,password))[:-1]
-    
+
     # device request message to send to server
     path = """/ws/FileData/~/%s/test/test.txt"""%(device_id)
-    
+
     # to what URL to send the request with a given HTTP method
     webservice = httplib.HTTP("login.etherios.com",80)
     webservice.putrequest("GET", path)
-    
+
     # add the authorization string into the HTTP header
     webservice.putheader("Authorization", "Basic %s"%auth)
     webservice.putheader("Accept", "text/html; charset=\"UTF-8\"")
     #webservice.putheader("Content-length", "%d" % len(message))
     webservice.endheaders()
     #webservice.send(message)
-    
+
     # get the response
     statuscode, statusmessage, header = webservice.getreply()
     response_body = webservice.getfile().read()
-    
+
     # print the output to standard out
     if statuscode == 200:
         print response_body
     else:
         print '\nError: %d %s' %(statuscode, statusmessage)
-    
+
     webservice.close()
 
 
@@ -60,8 +61,15 @@ def main(argv):
     if count != 3:
         Usage()
     else:
-        GetMessage(argv[0], argv[1], argv[2])
+        if len(argv[2]) == len("12345678-12345678"):
+            device_id = "00000000-00000000-" + argv[2]
+        else:
+            device_id = argv[2]
 
+        if re.match( "([0-9A-Fa-f]{8}-){3}[0-9A-Fa-f]{8}", device_id) == None:
+            print 'Error: Invalid device id [%s]' %argv[2]
+        else:
+            GetMessage(argv[0], argv[1], device_id)
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
