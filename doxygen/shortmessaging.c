e88d981b2b1192c61dd3d380c267c644896983f0
Paul Jacobson <pjacobson@digi.com>
2013-04-20 08:07:59 -0400

    New SHort messaging sections
    Signed-off-by: Paul Jacobson <pjacobson@digi.com>

--- doxygen/shortmessaging.c
+++ doxygen/shortmessaging.c
@@ -1,4 +1,4 @@
-/*! @page shortmessaging What is Short Messaging and why do I need it?
+/*! @page shortmessaging Short Messaging
  *
  * @htmlinclude nav.html
  *
@@ -9,13 +9,26 @@
  * on SSL. Any application can make use of these APIs to obtain secure connection
  * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
  *
- * @section smsection1 Why do I need Short Messaging?
+ * @section smsection2 Who needs Short Messaging?
  *
  * From the top level these callbacks are analogous to the @ref network_callbacks.
  * Internally they provide interface to securely connect to Etherios Device Cloud
  * on SSL. Any application can make use of these APIs to obtain secure connection
  * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
  *
+ * @section smsection3 How does Short Messaging solve my problem?
+ *
+ * From the top level these callbacks are analogous to the @ref network_callbacks.
+ * Internally they provide interface to securely connect to Etherios Device Cloud
+ * on SSL. Any application can make use of these APIs to obtain secure connection
+ * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
+ *
+ * @section smsection4 Short Messaging examples
+ *
+ * From the top level these callbacks are analogous to the @ref network_callbacks.
+ * Internally they provide interface to securely connect to Etherios Device Cloud
+ * on SSL. Any application can make use of these APIs to obtain secure connection
+ * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
  *
  * @htmlinclude terminate.html
  */
