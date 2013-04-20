fcb655d0b13d3f37e1531ef31eaaab72a42fa191
Paul Jacobson <pjacobson@digi.com>
2013-04-20 15:09:04 -0400

    Short Messaging Advanced page TOC.
    Signed-off-by: Paul Jacobson <pjacobson@digi.com>

--- doxygen/shortmessaging.c
+++ doxygen/shortmessaging.c
@@ -2,28 +2,84 @@
  *
  * @htmlinclude nav.html
  *
- * @section smsection1 What is Short Messaging?
+ * @section secsmintroduction Short Messaging Introduction
+ *
+ * Short Message (SM) is a lightweight, message oriented protocol that
+ * allows applications to optimize network data usage.
+ *
+ * This chapter will explain:
+ *
+ *   -# @ref smwhatisit
+ *        -# @ref smtransports
+ *   -# @ref smsectionwhoneedsit
+ *        -# @ref smsectionwhodoesnotneedsit
+ *   -# @ref smsolutions
+ *        -# @ref smcomplications
+ *   -# @ref smsecurity
+ *   -# @ref smsectionexamples
+ *
+ * @section smwhatisit What is Short Messaging?
+ *
+ * The Short Message (SM) protocol was designed to be a lightweight, message oriented
+ * protocol suitable for use on transports that are:
+ *    @li Expensive or where data usage needs to be kept to a minimum.
+ *    @li Lossy
+ *       -# Delivery not guaranteed
+ *       -# Duplicates possible
+ *       -# Data integrity not guaranteed
+ *
+ * The SM protocol supports the following commands: Reboot, Status Request (similar to a ping),
+ * and Data transfer.
+ *
+ * The SM protocol defines a set of encodings and supported commands and the expected behavior
+ * for those commands.  There are some aspects to the protocol that are transport specific.
+ *
+ * THe Etherios Cloud Connector supports only the UDP transport for Short Messaging.  In the
+ * future, SMS will be supported.
+ *
+ * @subsection smtransports Why is the transport important?
  *
  * From the top level these callbacks are analogous to the @ref network_callbacks.
  * Internally they provide interface to securely connect to Etherios Device Cloud
  * on SSL. Any application can make use of these APIs to obtain secure connection
  * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
  *
- * @section smsection2 Who needs Short Messaging?
+ * @section smsectionwhoneedsit Who needs Short Messaging?
  *
  * From the top level these callbacks are analogous to the @ref network_callbacks.
  * Internally they provide interface to securely connect to Etherios Device Cloud
  * on SSL. Any application can make use of these APIs to obtain secure connection
  * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
  *
- * @section smsection3 How does Short Messaging solve my problem?
+ * @subsection smsectionwhodoesnotneedsit Who doesn't need Short Messaging?
  *
  * From the top level these callbacks are analogous to the @ref network_callbacks.
  * Internally they provide interface to securely connect to Etherios Device Cloud
  * on SSL. Any application can make use of these APIs to obtain secure connection
  * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
  *
- * @section smsection4 Short Messaging examples
+ * @section smsolutions How does Short Messaging solve my problem?
+ *
+ * From the top level these callbacks are analogous to the @ref network_callbacks.
+ * Internally they provide interface to securely connect to Etherios Device Cloud
+ * on SSL. Any application can make use of these APIs to obtain secure connection
+ * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
+ *
+ * @subsection smcomplications How does Short Messaging complicate my application?
+ *
+ * From the top level these callbacks are analogous to the @ref network_callbacks.
+ * Internally they provide interface to securely connect to Etherios Device Cloud
+ * on SSL. Any application can make use of these APIs to obtain secure connection
+ * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
+ *
+ * @section smsecurity Short Messaging Security
+ *
+ * From the top level these callbacks are analogous to the @ref network_callbacks.
+ * Internally they provide interface to securely connect to Etherios Device Cloud
+ * on SSL. Any application can make use of these APIs to obtain secure connection
+ * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
+ *
+ * @section smsectionexamples Short Messaging examples
  *
  * From the top level these callbacks are analogous to the @ref network_callbacks.
  * Internally they provide interface to securely connect to Etherios Device Cloud
