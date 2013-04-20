5ae866f82045dfc83f4f39fc15560dee6e436765
Paul Jacobson <pjacobson@digi.com>
2013-04-20 17:06:28 -0400

    Short Messaging Advanced page TOC updates.
    Signed-off-by: Paul Jacobson <pjacobson@digi.com>

--- doxygen/shortmessaging.c
+++ doxygen/shortmessaging.c
@@ -79,7 +79,7 @@
  * on SSL. Any application can make use of these APIs to obtain secure connection
  * just by including network_tcp_ssl.c instead of network_tcp.c. The sample application
  *
- * @section smsectionexamples Short Messaging examples
+ * @section smsectionexamples Short Messaging Examples
  *
  * From the top level these callbacks are analogous to the @ref network_callbacks.
  * Internally they provide interface to securely connect to Etherios Device Cloud
