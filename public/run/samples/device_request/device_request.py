47b3ba5e776d3a8e576a1953a1a14fca4db76e2e
Paul Jacobson <pjacobson@digi.com>
2013-04-23 12:36:46 -0400

    IC4C-85 corrections
    Signed-off-by: Paul Jacobson <pjacobson@digi.com>

--- public/run/samples/device_request/device_request.py
+++ public/run/samples/device_request/device_request.py
@@ -14,14 +14,36 @@
 # device_request.py
 # Send SCI device_request to server. 
 # -------------------------------------------------
-# Usage: device_request.py <username> <password> <device_id> [<cloud_url>]
+# Usage: device_request.py <Username> <Password> <Device ID> [<Device Cloud URL>]
 # -------------------------------------------------
 import httplib
 import base64
 import sys
 
 def Usage():
-    print 'Usage: device_request.py <username> <password> <device_id> [<cloud_url>]\n'
+    print 'Usage: device_request.py <Username> <Password> <Device ID> [<Device Cloud URL>]'
+    print '    Sends an SCI Data Service-Device Request to <Device ID> with the'
+    print '    device_request target_name set to \"myTarget\" and the Device Request data'
+    print '    set to \"My device request data\".'
+    print '    where:' 
+    print '        <Username> is the Device Cloud for Etherios account Username to which your device is'
+    print '                   connected.'
+    print '        <Password> is the account password'
+    print '        <Device ID> is the device to receive the Device Request.' 
+    print '        [<Device Cloud URL>] is an optional Device Cloud URL.  The default URL is' 
+    print '                   login.etherios.com.' 
+    print '' 
+    print '    Note:'
+    print '        <Device ID> format can either be:'
+    print '            Long: 00000000-00000000-00049DFF-FFAABBCC.' 
+    print '            or short: 00049DFF-FFAABBCC\n'
+    print '    Example Usage:' 
+    print '        python ./device_request.py myaccount mypassword 00049DFF-FFAABBCC'
+    print '            Sends an SCI Data Service-Device Request to 00000000-00000000-00049DFF-FFAABBCC '
+    print '            (in user account myaccount) through login.etherios.com.\n'
+    print '        python ./device_request.py myaccount mypassword 00049DFF-FFAABBCC login.etherios.co.uk'
+    print '            Sends an SCI Data Service-Device Request to 00000000-00000000-00049DFF-FFAABBCC '
+    print '            (in user account myaccount) through login.etherios.co.uk.\n'
    
 def PostMessage(username, password, device_id, cloud_url):
     # create HTTP basic authentication string, this consists of
