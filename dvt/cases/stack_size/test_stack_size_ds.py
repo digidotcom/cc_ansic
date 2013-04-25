050eeec180458116ce7fed4f7a19b875996194d4
Ming Liu-Chan <Ming.LiuChan@digi.com>
2013-04-25 14:47:09 -0500

    Added 120 timeout for post in test_stack_size_ds.py
    Signed-off-by: Ming Liu-Chan <Ming.LiuChan@digi.com>

--- dvt/cases/stack_size/test_stack_size_ds.py
+++ dvt/cases/stack_size/test_stack_size_ds.py
@@ -1,3 +1,4 @@
+# -*- coding: iso-8859-1 -*-
 import xpath
 import time
 import ic_testcase
@@ -31,12 +32,12 @@ TARGET_DEVICE_REQUEST = \
 </sci_request>"""
 
 RCI_QUERY_STATE = \
-"""<sci_request version="1.0"> 
-  <send_message cache="false"> 
-    <targets> 
-      <device id="%s"/> 
-    </targets> 
-    <rci_request version="1.1"> 
+"""<sci_request version="1.0">
+  <send_message cache="false">
+    <targets>
+      <device id="%s"/>
+    </targets>
+    <rci_request version="1.1">
       <query_state/>
     </rci_request>
   </send_message>
@@ -45,10 +46,10 @@ RCI_QUERY_STATE = \
 class StackSizeTestCase(ic_testcase.TestCase):
 
     def test_stacksize_with_begin_rci(self):
-    
-        """ Sends query_setting. 
+
+        """ Sends query_setting.
         """
-        self.log.info("**** Stack Size DS Test:  start RCI query_state")
+        self.log.info("**** Beginnin Stack Size DS Test:  start RCI query_state")
         # Get the current value
 
         rci_request = (RCI_QUERY_STATE % self.device_id);
@@ -58,32 +59,32 @@ class StackSizeTestCase(ic_testcase.TestCase):
 
         self.log.info('Response from query state: %s' %rci_response)
 
-        # Parse request response 
+        # Parse request response
         dom = xml.dom.minidom.parseString(rci_response)
         rci_error_response = dom.getElementsByTagName('error')
-    
+
         if len(rci_error_response) != 0:
             self.log.info("Request: %s" % rci_request)
             self.log.info("Response: %s" % rci_response)
             self.assertTrue(error is found, "Got error response")
 
     def test_stacksize_with_ds(self):
-    
-        """ Sends device request. 
+
+        """ Sends device request.
         """
-        self.log.info("**** Stack Size DVT:  Device request")
+        self.log.info("**** Beginning Stack Size DVT:  Device request")
         self.valid_target("ds_stacksize_test", 2048)
         self.valid_target("put_ds_stacksize_test", 100)
         monitor = DeviceConnectionMonitor(self.push_client, self.dev_id)
 
         try:
             monitor.start()
-            
+
             if device_is_connected(self) == False:
                 self.log.info("Waiting for device to connect before start testing")
                 monitor.wait_for_connect(30)
             self.log.info("Device is connected. Start testing")
-            
+
             # Send device request to request terminate the sample
             device_request_response = self.send_device_request('request_terminate', 0)
             self.log.info("RESPONSE %s" %device_request_response)
@@ -96,15 +97,15 @@ class StackSizeTestCase(ic_testcase.TestCase):
             monitor.wait_for_connect(30)
             self.log.info("Device connected.")
         finally:
-            monitor.stop()        
+            monitor.stop()
 
 
     def invalid_target(self, my_target_name, data_len):
-    
-        """ Verifies that the device request returned match the expected 
+
+        """ Verifies that the device request returned match the expected
         response.
         """
- 
+
         # Send device request
         device_request_response = self.send_device_request(my_target_name, data_len)
         self.log.info("RESPONSE %s" %device_request_response)
@@ -112,10 +113,10 @@ class StackSizeTestCase(ic_testcase.TestCase):
             self.log.info("Service not available.")
             return "no_service"
 
-        # Parse request response 
+        # Parse request response
         dom = xml.dom.minidom.parseString(device_request_response)
         device_response = dom.getElementsByTagName("device_request")
-        
+
         # Validate cancel message response
         self.log.info("Determining if \"%s\" target is cancelled." % my_target_name)
         error = getText(device_response[0].getElementsByTagName('error')[0])
@@ -123,13 +124,13 @@ class StackSizeTestCase(ic_testcase.TestCase):
         cancelled_response = "Message transmission cancelled"
 
         self.assertEqual(error, cancelled_response,
-            "returned target (%s) is not (%s)" 
+            "returned target (%s) is not (%s)"
             % (error, cancelled_response))
         return "ok"
 
     def valid_target(self, my_target_name, data_len):
-    
-        """ Verifies that the device request returned match the expected 
+
+        """ Verifies that the device request returned match the expected
         response.
         """
         #if DS_SERVICE == 0:
@@ -141,37 +142,37 @@ class StackSizeTestCase(ic_testcase.TestCase):
             self.log.info("Service not available.")
             return "no_service"
 
-        # Parse request response 
+        # Parse request response
         dom = xml.dom.minidom.parseString(device_request_response)
         device_response = dom.getElementsByTagName("device_request")
-        
+
         # Validate target name
         self.log.info("Determining if the target_name is \"%s\"." % my_target_name)
         target_name = device_response[0].getAttribute('target_name')
         self.log.info("Target: %s " % target_name)
-        self.assertEqual(target_name, my_target_name, 
-            "returned target (%s) is not (%s)" 
+        self.assertEqual(target_name, my_target_name,
+            "returned target (%s) is not (%s)"
             % (target_name, my_target_name))
 
-        # Validate status 
+        # Validate status
         self.log.info("Determining if status is success.")
         status = device_response[0].getAttribute("status")
-        self.assertEqual(status, '0', 
-            "returned status (%s) is not success status" 
+        self.assertEqual(status, '0',
+            "returned status (%s) is not success status"
             % status)
         return "ok"
 
     def send_device_request(self, target_name, request_length):
-        
+
         request_data = self.get_random_word(request_length);
 
         request = (TARGET_DEVICE_REQUEST % (self.device_id, target_name, request_data))
- 
+
         self.log.info("Sending device request for \"%s\" target_name to server for device id  %s." % (target_name, self.device_id))
-        device_request_response = self.session.post('http://%s/ws/sci' % self.hostname, data=request).content
+        device_request_response = self.session.post('http://%s/ws/sci' % self.hostname, data=request, timeout=120).content
 
         return device_request_response
-    
+
     def get_random_word(self, wordLen):
         word = ''
         for i in range(wordLen):
