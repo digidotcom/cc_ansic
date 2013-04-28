42248616eff99c1e7bb01db56f0ae60dd8689aab
Mike Zarns <mikez@digi.com>
2013-04-28 20:35:19 +0200

    Misc rework

--- doxygen/shortmessaging.c
+++ doxygen/shortmessaging.c
@@ -4,42 +4,52 @@
  *
  * @section secsmintroduction Introduction
  *
- * TCP/IP communication includes overhead to establish, maintain, and close connections,
- * as well as overhead to ensure delivery and integrity.  The communication is reliable and
- * potentially @ref network_ssl_callbacks "secure".  Most Cloud Connector communication
- * covered in this guide occurs over TCP/IP.
+ * Short Messaging (SM) provides an alternative transport for communicating with the cloud and is 
+ * typically used only when data usage is costly and guaranteed data delivery is unnecessary. SM 
+ * uses UDP as a transport rather the TCP transport normally used in device-to-cloud communications.
  *
- * The Short Message (SM) protocol was designed to be a lightweight, message-based protocol
- * ideal for costly transports or for minimizing traffic.  SM transports over UDP and therefore
- * does not incur the TCP/IP connection overhead.  Unfortunately, UDP includes limitations
- * such as unreliable delivery, potential duplicate packets, and lack of data integrity.
- * Cloud Connector applications that include SM will require consideration and handling of
- * these UDP limitations.
+ * TCP communication includes overhead to establish, maintain, and close connections as well as 
+ * overhead to ensure delivery and integrity. The communication is reliable and can be 
+ * @ref network_ssl_callbacks secured. Most cloud communications discussed in this guide leverage 
+ * standard TCP communications.
+ * 
+ * SM communicates over UDP and therefore does not incur the TCP connection overhead. 
+ * Unfortunately, UDP includes limitations such as unreliable delivery and the potential for 
+ * duplicate packets. Implementations that include SM will require consideration and 
+ * handling of these UDP limitations Ð a price you may choose to pay to reduce data usage on costly 
+ * data networks such as cellular or satellite.
  *
+ * Most devices will be deployed on networks with few restrictions on data usage. For these 
+ * applications, TCP should be used and is the method demonstrated in all of the samples included in 
+ * this kit. These applications will establish a TCP connection and maintain that connection so that 
+ * the device is always securely accessible from the cloud.
+ * 
+ * Conversely, some devices could be deployed on networks that include data restrictions. Others may 
+ * be battery-powered and must minimize active network time. For this latter group, Short Messaging 
+ * should be considered.
+ * 
  * @section smsectionwhoneedsit Should I use Short Messaging?
- * Most Cloud Connector applications will be deployed on networks with few restrictions
- * on data usage.  For these applications, we highly recommend using the samples included in
- * this kit, that were intended for a @ref network_tcp_start "TCP/IP transport".  These applications
- * will establish a @ref app_start_network_tcp "TCP/IP connection" and maintain it throughout the
- * entire application's execution.
- *
- * Conversely, some Cloud Connector applications could be deployed on networks that include
- * data restrictions; or some Cloud Connector applications could be deployed on systems
- * with severe power limitations.
- *
- * For this latter group, Short Messaging should be considered.
+ * 
+ * Most devices will be deployed on networks with few restrictions on data usage. For these 
+ * applications, TCP should be used and is the method demonstrated in all of the samples included in 
+ * this kit. These applications will establish a TCP connection and maintain that connection so that 
+ * the device is always securely accessible from the cloud.
+ * 
+ * Conversely, some devices could be deployed on networks that include data restrictions. Others may 
+ * be battery-powered and must minimize active network time. For this latter group, Short Messaging 
+ * should be considered.
  *
  * @subsection smsolutions How does Short Messaging solve my problem?
  *
- * A Cloud Connector application need not always have a TCP/IP connection.  Further, some
- * Cloud Connector applications can eliminate this connection altogether.
+ * A device need not always have a TCP connection.  Further, some
  *
- * For example, a cellular based Cloud Connector application might send GPS data to Device Cloud
+ * devices can eliminate this connection altogether.
+ * For example, a cellular based device might send GPS data to Device Cloud
  * every few hours and then check for remote instructions.  Based on the instruction, the
- * application could then start a TCP/IP connection and perform a standard Cloud Connector
+ * application could then start a TCP connection and perform a standard Cloud Connector
  * operation.
  *
- * Alternatively, a battery based Cloud Connector application might power up, read a sensor,
+ * Alternatively, a battery based device might power up, read a sensor,
  * send the data to Device Cloud, check for pending @ref web_services "Web Service" instructions,
  * and then shut down if no instructions are queued.
  *
@@ -54,7 +64,7 @@
  *
  * <b>UDP Reliability </b>
  *
- * Depending on what you optimize, a Cloud Connector application will need to consider
+ * Depending on what you optimize, a device will need to consider
  * the unreliability of UDP.
  *
  * Suppose your application samples data periodically and sends to Device Cloud once
@@ -69,7 +79,7 @@
  * @ref web_services application will need to handle this case.
  *
  * Suppose data integrity is a critical requirement.  In this case, a Cloud Connector
- * application might choose to start a TCP/IP connection and send the data reliably
+ * application might choose to start a TCP connection and send the data reliably
  * instead of using the UDP transport.
  *
  * Cloud Connector has the flexibility to support either case.
@@ -81,7 +91,7 @@
  * on Device Cloud, waiting for a Cloud Connector message.
  *
  * Cloud Connector @b must @b always @b initiate SM exchanges.  A key component of
- * your Cloud Connector Application must include a strategy to open the firewall and
+ * your implementation must include a strategy to open the firewall and
  * drain the SM requests.
  *
  * If Cloud Connector has no pending message to send, the application can @ref initiate_ping_to_cloud.
@@ -101,7 +111,7 @@
  *
  * @section ping_request Ping Device Cloud
  *
- * A Cloud Connector Application can send a @ref initiate_ping_to_cloud "Ping request to Device Cloud" at any
+ * A device can send a @ref initiate_ping_to_cloud "Ping request to Device Cloud" at any
  * time.  The purpose is to open a hole in a firewall and let Device Cloud know that the Application is
  * ready to receive pending or queued messages.
  *
@@ -137,12 +147,12 @@
  *   <td>request_data</td>
  *   <td>Pointer to @endhtmlonly connector_sm_send_ping_request_t @htmlonly structure, where member:
  *      <ul>
- *        <li><b><i>transport</i></b>: Ping Request transport.  See @endhtmlonly @ref connector_transport_udp. @htmlonly </li>
- *        <li><b><i>user_context</i></b>: An opaque application defined context.  This pointer is returned
- *                                        during a @endhtmlonly @ref ping_response_callback "response callback" @htmlonly and used
- *                                        to identify the origin of the response. </li>
- *        <li><b><i>response_required</i></b>: Set to @endhtmlonly @ref connector_true if a @ref ping_response_callback " response callback" is required.
- *                                             Set to @ref connector_false when no response callback is required.  @htmlonly </li>
+ *        <li><b><i>transport</i></b>: Ping Request transport.  Set to @endhtmlonly @ref connector_transport_udp . @htmlonly </li>
+ *        <li><b><i>transport</i></b>: Ping Request transport.  Set to @endhtmlonly @ref connector_transport_udp. @htmlonly </li>
+ *        <li><b><i>user_context</i></b>: An opaque application-defined context.  This pointer is returned
+ *                                        during a @endhtmlonly @ref ping_response_callback "response callback". @htmlonly </li>
+ *        <li><b><i>response_required</i></b>: Set to @endhtmlonly @ref connector_true if notification of ping receipt is needed,
+ *                                             otherwise set to @ref connector_false.  See @ref ping_response_callback . @htmlonly </li>
  *      </ul>
  *    </td>
  * </tr>
@@ -170,7 +180,6 @@
  *
  *   ping_request.transport = connector_transport_udp;
  *   ping_request.response_required = connector_true;
- *   ping_request.user_context = &ping_request;
  *
  *   // Send ping to Device Cloud
  *   status = connector_initiate_action(handle, connector_initiate_ping_request, &ping_request);
@@ -179,10 +188,7 @@
  * This example will invoke Cloud Connector to initiate a ping request to Device Cloud. The completion
  * of this request is indicated by a @ref ping_response_callback "response callback".
  *
- * @note The variable ping_request above is not a stack variables. Either you can use a heap or
- * a memory (global or static) variable to hold this value. You can release them when you get a
- * @ref  ping_response_callback "response" callback. The value passed as the user_context will
- * be returned in the response.
+ * @note The ping_request variable must remain in scope or allocated until the @ref ping_response_callback "response" callback. 
  *
  * @see @ref connector_initiate_action()
  * @see @ref ping_response_callback
@@ -190,10 +196,15 @@
  *
  * @subsection ping_response_callback  Ping Device Cloud Response callback
  *
- * When a @ref initiate_ping_to_cloud operation has the @b response_required set to @ref connector_true,
- * a @b ping_response_callback is made when a reply to the original Ping is received.  This callback
- * has a @ref connector_request_id_sm_ping_response Request ID and with the data cast as
- * a @ref connector_sm_ping_response_t with status set to @b connector_sm_ping_status_success .
+ * The ping_response_callback is made for one of the following conditions: 
+ * the ping was successfully sent; the ping request has been canceled; a ping response has been received
+ * from the cloud, the ping timed out, or an error occurred. 
+ *
+ * @note When @ref initiate_ping_to_cloud operation has the response_required set @ref connector_false, this callback
+ * is made with the @ref connector_sm_ping_response_t status set to @b connector_sm_ping_status_complete when the
+ * message is sent.  If the @ref initiate_ping_to_cloud operation is @ref cancel_session "canceled" before the
+ * message is sent, this callback is made with the @ref connector_sm_ping_response_t status is set to
+ * @b connector_sm_ping_status_cancel.
  *
  * @htmlonly
  * <table class="apitable">
@@ -236,11 +247,8 @@
  * </table>
  * @endhtmlonly
  *
- * @note When @ref initiate_ping_to_cloud operation has the response_required set @ref connector_false, this callback
- * is made with the @ref connector_sm_ping_response_t status set to @b connector_sm_ping_status_complete when the
- * message is sent.  If the @ref initiate_ping_to_cloud operation is @ref cancel_session "canceled" before the
- * message is sent, this callback is made with the @ref connector_sm_ping_response_t status is set to
- * @b connector_sm_ping_status_cancel.
+ * @note The response code connector_sm_ping_status_success will only be received if the @ref initiate_ping_to_cloud request has the 
+ * @b response_required set to @ref connector_true and a ping response has been received from the cloud. 
  *
  * @see @ref initiate_ping_to_cloud
  *
@@ -347,7 +355,7 @@
  *   <td>Pointer to @endhtmlonly connector_sm_cli_request_t @htmlonly structure:
  *     <ul>
  *       <li><b><i>transport</i></b>: Transport where Message was received.  See @endhtmlonly @ref connector_transport_udp. @htmlonly</li>
- *       <li><b><i>user_context</i></b>: An opaque application defined context.  This pointer is passed to all subsequent
+ *       <li><b><i>user_context</i></b>: An opaque application-defined context.  This pointer is passed to all subsequent
  *                                        callbacks during the CLI sequence for this command.
  *       <li><b><i>buffer</i></b>: Buffer containing the Device Cloud CLI command. </li>
  *       <li><b><i>bytes_used</i></b>: Number of bytes used for the CLI command in the buffer. </li>
@@ -413,7 +421,7 @@
  *   <td>Pointer to @endhtmlonly connector_sm_cli_response_length_t @htmlonly structure:
  *     <ul>
  *       <li><b><i>transport</i></b>: Transport where initial CLI request was received.  See @endhtmlonly @ref connector_transport_udp. @htmlonly</li>
- *       <li><b><i>user_context</i></b>: The opaque application defined context passed in from the @endhtmlonly @ref cli_request_callback @htmlonly </li>
+ *       <li><b><i>user_context</i></b>: The opaque application-defined context passed in from the @endhtmlonly @ref cli_request_callback @htmlonly </li>
  *       <li><b><i>total_bytes</i></b>: Maximum size in bytes of the CLI response to the initial @endhtmlonly @ref cli_request_callback @htmlonly </li>
  *     </ul>
  *   </td>
@@ -473,7 +481,7 @@
  *   <td>Pointer to @endhtmlonly connector_sm_cli_response_t @htmlonly structure:
  *     <ul>
  *       <li><b><i>transport</i></b>: Transport where initial CLI request was received.  See @endhtmlonly @ref connector_transport_udp. @htmlonly</li>
- *       <li><b><i>user_context</i></b>: The opaque application defined context passed in from the @endhtmlonly @ref cli_request_callback @htmlonly </li>
+ *       <li><b><i>user_context</i></b>: The opaque application-defined context passed in from the @endhtmlonly @ref cli_request_callback @htmlonly </li>
  *       <li><b><i>buffer</i></b>: the memory to copy your CLI response. </li>
  *       <li><b><i>bytes_available</i></b>: buffer size available in bytes. </li>
  *       <li><b><i>bytes_used</i></b>: number of bytes copied by the callback.</li>
@@ -539,7 +547,7 @@
  *   <td>Pointer to @endhtmlonly connector_sm_cli_status_t @htmlonly structure:
  *     <ul>
  *       <li><b><i>transport</i></b>: Transport where initial CLI request was received.  See @endhtmlonly @ref connector_transport_udp. @htmlonly</li>
- *       <li><b><i>user_context</i></b>: The opaque application defined context passed in from the @endhtmlonly @ref cli_request_callback @htmlonly </li>
+ *       <li><b><i>user_context</i></b>: The opaque application-defined context passed in from the @endhtmlonly @ref cli_request_callback @htmlonly </li>
  *       <li><b><i>status</i></b>: @endhtmlonly
  *                                 @b connector_sm_cli_status_cancel if @ref connector_initiate_stop_request_t "transport stopped"
  *                                 @b connector_sm_cli_status_error if resource allocation failure. @htmlonly
@@ -570,12 +578,12 @@
  *
  * @subsection sm_connect Request TCP start
  *
- * Requests the Cloud Connector to start it's TCP/IP transport.  This request will
+ * Requests the Cloud Connector to start it's TCP transport.  This request will
  * be handled in the Cloud Connector private layer will start the TCP.
  *
- * Once the TCP transport @ref network_tcp_start "is started", Cloud Connector Applications can make use of
+ * Once the TCP transport @ref network_tcp_start "is started", devices can make use of
  * the TCP features like @ref firmware_download, @ref rci_service, or reliable
- * @ref data_point or @ref data_service over a @ref connector_transport_tcp "TCP/IP transport".
+ * @ref data_point or @ref data_service over a @ref connector_transport_tcp "TCP transport".
  *
  * The following @ref web_services example shows how to request a device to start it's TCP service:
  *
