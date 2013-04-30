997fb31f737e184b1d3fe00e15be4a7adba3b176
Hector Bujanda <hector.bujanda@digi.com>
2013-04-30 17:16:21 +0200

    Extended to link ucos default project, BSP and OS files

--- kits/kinetis/scripts/build_linked.bat
+++ kits/kinetis/scripts/build_linked.bat
@@ -5,7 +5,10 @@ cls
 SET ROOT_DIR=C:\Etherios4Kinetis
 SET EC_LIB_DIR=%ROOT_DIR%\Etherios_CC
 SET CW_WORKSPACE=%ROOT_DIR%\ecc_cw_project
-SET IAR_WORKSPACE=%ROOT_DIR%\ecc_iar_project
+SET MQX_IAR_WORKSPACE=%ROOT_DIR%\ecc_iar_project
+SET UCOS_IAR_WORKSPACE=%ROOT_DIR%\uCOS
+REM UCOS_REPO_DIR must point to the ucos repository's root directory
+SET UCOS_REPO_DIR=C:\Git_root_c\ucos
 REM BASE_DIR must point to the repository's root directory
 SET BASE_DIR=..\..\..
 
@@ -19,7 +22,8 @@ popd
 
 RMDIR /S /Q "%EC_LIB_DIR%"
 RMDIR /S /Q "%CW_WORKSPACE%"
-RMDIR /S /Q "%IAR_WORKSPACE%"
+RMDIR /S /Q "%MQX_IAR_WORKSPACE%"
+RMDIR /S /Q "%UCOS_IAR_WORKSPACE%"
 
 MKDIR %ROOT_DIR%
 MKDIR %ROOT_DIR%\utils
@@ -36,10 +40,23 @@ MKDIR "%CW_WORKSPACE%\etherios_app"
 MKDIR "%CW_WORKSPACE%\etherios_app\Sources"
 MKDIR "%CW_WORKSPACE%\etherios_app\Sources\fs_devices"
 
-MKDIR "%IAR_WORKSPACE%"
-MKDIR "%IAR_WORKSPACE%\etherios_app"
-MKDIR "%IAR_WORKSPACE%\etherios_app\Sources"
-MKDIR "%IAR_WORKSPACE%\etherios_app\Sources\fs_devices"
+MKDIR "%MQX_IAR_WORKSPACE%"
+MKDIR "%MQX_IAR_WORKSPACE%\etherios_app"
+MKDIR "%MQX_IAR_WORKSPACE%\etherios_app\Sources"
+MKDIR "%MQX_IAR_WORKSPACE%\etherios_app\Sources\fs_devices"
+
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\etherios_projects\etherios_app"
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\BSP"
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\BSP\ucos-iii"
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\BSP\Freescale"
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\BSP\uCTCPIP-V2"
+
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\etherios_projects\etherios_app"
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\BSP"
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\BSP\ucos-iii"
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\BSP\Freescale"
+MKDIR "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\BSP\uCTCPIP-V2"
 
 
 REM Link the library
@@ -139,20 +156,75 @@ for %%F in ("%BASE_DIR%\kits\kinetis\mqx\source\fs_devices\*.*") do (
 	mklink /H "%CW_WORKSPACE%\etherios_app\Sources\fs_devices\%%~nxF" "%%F"
 )
 
-REM Link the template application in IAR workspace.
+REM Link the mqx template application in IAR workspace.
 for %%F in ("%BASE_DIR%\kits\kinetis\mqx\projects\iar\etherios_app\*.*") do (
-	mklink /H "%IAR_WORKSPACE%\etherios_app\%%~nxF" "%%F"
+	mklink /H "%MQX_IAR_WORKSPACE%\etherios_app\%%~nxF" "%%F"
 )
 for %%F in ("%BASE_DIR%\kits\kinetis\mqx\projects\iar\etherios_app\Sources\*.*") do (
-	mklink /H "%IAR_WORKSPACE%\etherios_app\Sources\%%~nxF" "%%F"
+	mklink /H "%MQX_IAR_WORKSPACE%\etherios_app\Sources\%%~nxF" "%%F"
 )
-mklink /H "%IAR_WORKSPACE%\etherios_app\Sources\main.c" "%BASE_DIR%\public\run\platforms\freescale\main.c"
-mklink /H "%IAR_WORKSPACE%\etherios_app\Sources\main.h" "%BASE_DIR%\public\run\platforms\freescale\main.h"
-mklink /H "%IAR_WORKSPACE%\etherios_app\Sources\application.c" "%BASE_DIR%\kits\kinetis\mqx\samples\send_data\application.c"
+mklink /H "%MQX_IAR_WORKSPACE%\etherios_app\Sources\main.c" "%BASE_DIR%\public\run\platforms\freescale\main.c"
+mklink /H "%MQX_IAR_WORKSPACE%\etherios_app\Sources\main.h" "%BASE_DIR%\public\run\platforms\freescale\main.h"
+mklink /H "%MQX_IAR_WORKSPACE%\etherios_app\Sources\application.c" "%BASE_DIR%\kits\kinetis\mqx\samples\send_data\application.c"
 
 for %%F in ("%BASE_DIR%\kits\kinetis\mqx\source\fs_devices\*.*") do (
-	mklink /H "%IAR_WORKSPACE%\etherios_app\Sources\fs_devices\%%~nxF" "%%F"
+	mklink /H "%MQX_IAR_WORKSPACE%\etherios_app\Sources\fs_devices\%%~nxF" "%%F"
 )
+
+REM Link the ucos template application in IAR workspace.
+for %%F in ("%BASE_DIR%\kits\kinetis\ucos\projects\iar\etherios_app\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\etherios_projects\etherios_app\%%~nxF" "%%F"
+)
+for %%F in ("%BASE_DIR%\kits\kinetis\ucos\projects\iar\etherios_app\Sources\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\etherios_projects\etherios_app\%%~nxF" "%%F"
+)
+
+for %%F in ("%BASE_DIR%\kits\kinetis\ucos\projects\iar\etherios_app\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\etherios_projects\etherios_app\%%~nxF" "%%F"
+)
+for %%F in ("%BASE_DIR%\kits\kinetis\ucos\projects\iar\etherios_app\Sources\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\etherios_projects\etherios_app\%%~nxF" "%%F"
+)
+
+REM Following lines links all necessary folders from the Micrium official package directory
+REM Micrium official package directory CAN'T be distributed to clients
+SET MICRIUM_OFFICIAL_PACKAGE_DIR="%UCOS_REPO_DIR%\Micrium_Official_Packackages"
+
+mklink /D "%UCOS_IAR_WORKSPACE%\Micrium\Software\uCOS-III" "%MICRIUM_OFFICIAL_PACKAGE_DIR%\KRN-K3XX-PKG000-X-P1\Micrium\Software\uCOS-III"
+mklink /D "%UCOS_IAR_WORKSPACE%\Micrium\Software\uC-LIB" "%MICRIUM_OFFICIAL_PACKAGE_DIR%\KRN-K3XX-PKG000-X-P1\Micrium\Software\uC-LIB"
+mklink /D "%UCOS_IAR_WORKSPACE%\Micrium\Software\uC-CPU" "%MICRIUM_OFFICIAL_PACKAGE_DIR%\KRN-K3XX-PKG000-X-P1\Micrium\Software\uC-CPU"
+
+mklink /D "%UCOS_IAR_WORKSPACE%\Micrium\Software\uC-TCPIP-V2" "%MICRIUM_OFFICIAL_PACKAGE_DIR%\NET-TCPX-PKG000-X-P1\Micrium\Software\uC-TCPIP-V2"
+mklink /D "%UCOS_IAR_WORKSPACE%\Micrium\Software\uC-DHCPc"    "%MICRIUM_OFFICIAL_PACKAGE_DIR%\NET-TCPX-PKG000-X-P1\Micrium\Software\uC-DHCPc"
+mklink /D "%UCOS_IAR_WORKSPACE%\Micrium\Software\uC-DNSc"     "%MICRIUM_OFFICIAL_PACKAGE_DIR%\NET-TCPX-PKG000-X-P1\Micrium\Software\uC-DNSc"
+
+REM Following lines links ucos BSPs
+SET MICRIUM_BSPS_DIR="%UCOS_REPO_DIR%\BSPs"
+for %%F in ("%MICRIUM_BSPS_DIR%\ucos-iii\TWRK53N512\BSP\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\BSP\%%~nxF" "%%F"
+)
+for %%F in ("%MICRIUM_BSPS_DIR%\ucos-iii\TWRK53N512\BSP\ucos-iii\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\BSP\ucos-iii\%%~nxF" "%%F"
+)
+for %%F in ("%MICRIUM_BSPS_DIR%\ucos-iii\TWRK53N512\BSP\Freescale\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\BSP\Freescale\%%~nxF" "%%F"
+)
+for %%F in ("%MICRIUM_BSPS_DIR%\ucos-iii\TWRK53N512\BSP\uCTCPIP-V2\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K53N512\IAR\BSP\uCTCPIP-V2\%%~nxF" "%%F"
+)
+for %%F in ("%MICRIUM_BSPS_DIR%\ucos-iii\TWRK60N512\BSP\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\BSP\%%~nxF" "%%F"
+)
+for %%F in ("%MICRIUM_BSPS_DIR%\ucos-iii\TWRK60N512\BSP\ucos-iii\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\BSP\ucos-iii\%%~nxF" "%%F"
+)
+for %%F in ("%MICRIUM_BSPS_DIR%\ucos-iii\TWRK60N512\BSP\Freescale\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\BSP\Freescale\%%~nxF" "%%F"
+)
+for %%F in ("%MICRIUM_BSPS_DIR%\ucos-iii\TWRK60N512\BSP\uCTCPIP-V2\*.*") do (
+	mklink /H "%UCOS_IAR_WORKSPACE%\Micrium\Software\EvalBoards\Freescale\TWR-K60N512\IAR\BSP\uCTCPIP-V2\%%~nxF" "%%F"
+)
+
 
 mklink /H "%ROOT_DIR%\utils\FWUpdateIntFlash.lcf" "%BASE_DIR%\kits\kinetis\mqx\projects\misc\FWUpdateIntFlash.lcf"
 
