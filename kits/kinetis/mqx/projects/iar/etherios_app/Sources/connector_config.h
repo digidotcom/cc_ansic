c98d762595556f65d9c745abee19e3d4dcdb27d5
Sebastian Pastor <sebastian.pastor@digi.com>
2013-05-23 20:02:34 +0200

    etherios_app: updated connector_config.h

--- kits/kinetis/mqx/projects/iar/etherios_app/Sources/connector_config.h
+++ kits/kinetis/mqx/projects/iar/etherios_app/Sources/connector_config.h
@@ -3,21 +3,22 @@
  */
 
 /* General category */
-#define CONNECTOR_MAC_ADDRESS                         {0x00, 0x04, 0x9F, 0x01, 0xA0, 0x60}
-#define CONNECTOR_VENDOR_ID                           0x02001371
-#define CONNECTOR_CLOUD_URL                           "test.etherios.com"
-#define CONNECTOR_DEVICE_TYPE                         "MQX 4.0 on Kinetis"
+//#define CONNECTOR_MAC_ADDRESS                         {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+//#define CONNECTOR_VENDOR_ID                           0x00000000
+//#define CONNECTOR_CLOUD_URL                           "login.etherios.com"
+#define CONNECTOR_DEVICE_TYPE                         	"MQX 4.0 on Kinetis"
 
 /* Services category */
-//#define CONNECTOR_MSG_MAX_TRANSACTION                 1
+#define CONNECTOR_MSG_MAX_TRANSACTION                 1
 //#define CONNECTOR_DATA_POINTS
-//#define CONNECTOR_DATA_SERVICE
+#define CONNECTOR_DATA_SERVICE
 
 
 //#define CONNECTOR_FILE_SYSTEM
 //#define CONNECTOR_FILE_SYSTEM_MAX_PATH_LENGTH   256
-//#define FILE_SYSTEM_USB 		/* Define only one */
-//#define FILE_SYSTEM_SDCARD	/* Define only one */
+//#define APPLICATION_FILE_SYSTEM_USB 		/* Define only one */
+//#define APPLICATION_FILE_SYSTEM_SDCARD	/* Define only one */
+//#define APPLICATION_IGNORE_SDCARD_PROTECTION /* Define if compiling for TWRK60N512 RevD */
 
 //#define CONNECTOR_RCI_SERVICE
 //#define CONNECTOR_RCI_MAXIMUM_CONTENT_LENGTH 	256
@@ -32,18 +33,6 @@
 #define CONNECTOR_WAIT_COUNT                          5
 #define CONNECTOR_NO_COMPRESSION
 /* * * * * * * * * * * * * * * * * * * * */
-#ifndef CONNECTOR_MAC_ADDRESS
-#error "Please define a MAC address in connector_config.h"
-#endif
-
-#ifndef CONNECTOR_VENDOR_ID
-#error "Please define the Vendor ID in connector_config.h"
-#endif
-
-#ifndef CONNECTOR_VENDOR_ID
-#error "Please define the Device Cloud URL connector_config.h"
-#endif
-
 #if defined CONNECTOR_FIRMWARE_SERVICE
 #if !defined APPLICATION_FIRMWARE_SERVICE_STUB && !defined APPLICATION_FIRMWARE_SERVICE_FULL
 #error "You must define APPLICATION_FIRMWARE_SERVICE_STUB or APPLICATION_FIRMWARE_SERVICE_FULL in connector_config.h to support Firmware Updates"
