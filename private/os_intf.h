0a766bbad3b4e19fdddb4992d3dce00f7bb25303
Sebastian Pastor <sebastian.pastor@gmail.com>
2013-08-16 17:40:24 +0200

    IC4C-144: assert on connector_callback_unrecognized.
    	- Fixes: https://jira.digi.com/browse/IC4C-144

--- private/os_intf.h
+++ private/os_intf.h
@@ -76,6 +76,7 @@ static connector_callback_status_t connector_callback(connector_callback_t const
             status = connector_callback_abort;
             break;
     }
+    ASSERT(status != connector_callback_unrecognized);
 
     return status;
 }
