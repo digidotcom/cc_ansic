digidotcom/cc_ansic

commit 6920fa246ab1519d3aa8dadfe995252a0b50baec

IC4C-103: avoid reading invalid pointers.

private/connector_sm_session.h
@@ -184,16 +184,14 @@ static connector_status_t sm_cancel_session(connector_data_t * const connector_p
     connector_status_t result = connector_working;
     connector_sm_session_t * session = sm_ptr->session.head;
     connector_bool_t cancel_all = connector_bool(request_id == NULL);
-
-    if (cancel_all != connector_true && *request_id == SM_INVALID_REQUEST_ID)
-        goto done;
-
+    
     while (session != NULL)
     {
         connector_status_t status;
         connector_sm_session_t * next_session = NULL;
+        uint32_t session_request_id = request_id != NULL ? *request_id : SM_INVALID_REQUEST_ID;
 
-        if (cancel_all || (session->request_id == *request_id))
+        if (cancel_all || (session_request_id == *request_id))
         {
             if (session->user.context != NULL)
             {

commit 7812f4a03104998cacf498c311cf3bfe070fc402

connector_sm_session.h: removed unused label.
	- Error introduced in commit 6920fa246ab1519d3aa8dadfe995252a0b50baec

private/connector_sm_session.h
@@ -217,7 +217,6 @@ static connector_status_t sm_cancel_session(connector_data_t * const connector_p
         session = next_session != NULL ? next_session : session->next;
     }
 
-done:
     return result;
 }
 #else