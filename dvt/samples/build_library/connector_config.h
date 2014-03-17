cbac2e977b94cef382789f347ed955e1f21da8f3
Hector Bujanda <hector.bujanda@digi.com>
2014-03-17 13:14:55 +0100

    Changes on old DVTs for: CONNECTOR_SM_MAX_RX_SEGMENTS split into CONNECTOR_SM_UDP_MAX_RX_SEGMENTS and CONNECTOR_SM_SMS_MAX_RX_SEGMENTS

--- dvt/samples/build_library/connector_config.h
+++ dvt/samples/build_library/connector_config.h
@@ -29,7 +29,8 @@
 #define CONNECTOR_SM_UDP_MAX_SESSIONS   4
 #define CONNECTOR_SM_SMS_MAX_SESSIONS   4
 #define CONNECTOR_SM_MULTIPART
-#define CONNECTOR_SM_MAX_RX_SEGMENTS   12
+#define CONNECTOR_SM_UDP_MAX_RX_SEGMENTS   12
+#define CONNECTOR_SM_SMS_MAX_RX_SEGMENTS   12
 #define CONNECTOR_SM_TIMEOUT        0  /* In seconds, 0: Wait forever for the complete request/response */
 
 #define CONNECTOR_DATA_POINTS 
