8aa3119b5b54fdb2e4287ee7f16461292ff25bbc
Hector Bujanda <hector.bujanda@digi.com>
2014-03-20 09:33:30 +0100

    Cosmetics: Old DVT changes for: SM_WAIT_FOREVER definition moved from private to API.

--- dvt/samples/sm_udp_multipart/connector_config.h
+++ dvt/samples/sm_udp_multipart/connector_config.h
@@ -27,7 +27,7 @@
 #define CONNECTOR_SM_UDP_MAX_SESSIONS   8
 #define CONNECTOR_SM_MULTIPART
 #define CONNECTOR_SM_UDP_MAX_RX_SEGMENTS   12
-#define CONNECTOR_SM_UDP_RX_TIMEOUT        30  /* In seconds, 0: Wait forever for the complete request/response */
+#define CONNECTOR_SM_UDP_RX_TIMEOUT        30  /* In seconds, SM_WAIT_FOREVER: Wait forever for the complete request/response */
 #define CONNECTOR_SM_TIMEOUT_TX CONNECTOR_SM_UDP_RX_TIMEOUT /* Use same timeout as for RX */
 
 #define CONNECTOR_RCI_MAXIMUM_CONTENT_LENGTH    256
