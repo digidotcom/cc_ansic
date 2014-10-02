af43a1418fa8468255d5b5f3aa79929d8bf0483a
Vidya Shankar <vidya.shankar@digi.com>
2014-10-02 13:31:14 -0400

    Coverity 27803 fix.

--- private/rci_binary_callback.h
+++ private/rci_binary_callback.h
@@ -189,7 +189,8 @@ STATIC connector_bool_t rci_callback(rci_t * const rci)
             }
             case connector_request_id_remote_config_configurations:
                 ASSERT(remote_config_request != connector_request_id_remote_config_configurations);
-                break;
+                callback_complete = connector_false;
+                goto done;
             }
             break;            
 
@@ -360,6 +361,7 @@ STATIC connector_bool_t rci_callback(rci_t * const rci)
 #endif
     }
 
+done:
     return callback_complete;
 }
 
