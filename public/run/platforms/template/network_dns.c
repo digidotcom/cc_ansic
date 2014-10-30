d62972d949d99d016f7932b2d0c87399a5bea475
Sebastian Pastor <sebastian.pastor@digi.com>
2014-10-30 20:38:29 +0100

    Added network_dns.[ch] to template platform.

--- public/run/platforms/template/network_dns.c
+++ public/run/platforms/template/network_dns.c
@@ -9,10 +9,42 @@
  * Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
  * =======================================================================
  */
-/**
- * @file
- *  @brief DNS routines for Cloud Connector.
- */
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+
 #include "connector_api.h"
 #include "platform.h"
+#include "network_dns.h"
+
+#if defined (CONNECTOR_TRANSPORT_TCP) || (defined CONNECTOR_TRANSPORT_UDP)
+
+void app_dns_set_redirected(connector_class_id_t const class_id, int const state)
+{
+    UNUSED_ARGUMENT(class_id);
+    UNUSED_ARGUMENT(state);
+}
+
+void app_dns_cache_invalidate(connector_class_id_t const class_id)
+{
+    UNUSED_ARGUMENT(class_id);
+}
+
+connector_callback_status_t app_dns_resolve(connector_class_id_t const class_id,
+                                       char const * const device_cloud_url,
+                                       in_addr_t * const ip_addr)
+{
+    connector_callback_status_t status = connector_callback_continue;
+
+    UNUSED_ARGUMENT(class_id);
+    UNUSED_ARGUMENT(device_cloud_url);
+    UNUSED_ARGUMENT(ip_addr);
+    return status;
+}
+#endif
 
