b95c5413649355c23155b239ad81da41418a60e8
Hector Bujanda <hector.bujanda@digi.com>
2015-02-11 09:28:22 +0100

    cosmetics

--- private/rci_binary_error.h
+++ private/rci_binary_error.h
@@ -109,27 +109,11 @@ STATIC void rci_generate_error(rci_t * const rci)
 
                 switch (remote_config_request)
                 {
-                    case connector_request_id_remote_config_action_start:
-                        trigger_rci_callback(rci, connector_request_id_remote_config_action_end);
-                        break;
                     case connector_request_id_remote_config_session_start:
                         trigger_rci_callback(rci, connector_request_id_remote_config_session_end);
                         break;
-                    case connector_request_id_remote_config_action_end:
-                        if (remote_config->error_id < connector_rci_error_COUNT)
-                        {
-                            {
-                                connector_bool_t const overflow = rci_output_terminator(rci);
-                                if (overflow) goto done;
-                            }
-                            trigger_rci_callback(rci, connector_request_id_remote_config_session_end);
-                        }
-                        else
-                        {
-                            set_rci_error_state(rci, rci_error_state_id);
-                            remote_config->error_id = 0;
-                            state_call(rci, rci_parser_state_output);
-                        }
+                    case connector_request_id_remote_config_action_start:
+                        trigger_rci_callback(rci, connector_request_id_remote_config_action_end);
                         break;
                     case connector_request_id_remote_config_group_start:
                         if (remote_config->error_id < connector_rci_error_COUNT)
@@ -146,7 +130,6 @@ STATIC void rci_generate_error(rci_t * const rci)
                             state_call(rci, rci_parser_state_traverse);
                         }
                         break;
-
                     case connector_request_id_remote_config_group_process:
                         if (remote_config->error_id < connector_rci_error_COUNT)
                         {
@@ -176,10 +159,9 @@ STATIC void rci_generate_error(rci_t * const rci)
                     case connector_request_id_remote_config_group_end:
                         if (remote_config->error_id < connector_rci_error_COUNT)
                         {
-                            {
-                                connector_bool_t const overflow = rci_output_terminator(rci);
-                                if (overflow) goto done;
-                            }
+                            connector_bool_t const overflow = rci_output_terminator(rci);
+                            if (overflow) goto done;
+
                             trigger_rci_callback(rci, connector_request_id_remote_config_action_end);
                         }
                         else
@@ -189,11 +171,24 @@ STATIC void rci_generate_error(rci_t * const rci)
                             state_call(rci, rci_parser_state_output);
                         }
                         break;
+                    case connector_request_id_remote_config_action_end:
+                        if (remote_config->error_id < connector_rci_error_COUNT)
+                        {
+                            connector_bool_t const overflow = rci_output_terminator(rci);
+                            if (overflow) goto done;
 
+                            trigger_rci_callback(rci, connector_request_id_remote_config_session_end);
+                        }
+                        else
+                        {
+                            set_rci_error_state(rci, rci_error_state_id);
+                            remote_config->error_id = 0;
+                            state_call(rci, rci_parser_state_output);
+                        }
+                        break;
                     case connector_request_id_remote_config_session_cancel:
                         ASSERT(connector_false);
                         break;
-
                     case connector_request_id_remote_config_session_end:
                         rci->status = rci_status_complete;
                         break;
